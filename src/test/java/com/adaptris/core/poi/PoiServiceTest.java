7687f9db6af38fc52e97c6fb12f3223b15b7ad6d
Sebastien Belin <sebastien.belin@adaptris.com>
2020-06-02 07:46:06 +0100

    INTERLOK-3181 #comment Add tests

--- src/test/java/com/adaptris/core/poi/PoiServiceTest.java
+++ src/test/java/com/adaptris/core/poi/PoiServiceTest.java
@@ -1,13 +1,18 @@
 package com.adaptris.core.poi;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import javax.xml.xpath.XPathExpressionException;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
@@ -16,14 +21,16 @@ import org.w3c.dom.Document;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.DefaultMessageFactory;
 import com.adaptris.core.ServiceCase;
+import com.adaptris.core.ServiceException;
 import com.adaptris.core.util.DocumentBuilderFactoryBuilder;
 import com.adaptris.core.util.XmlHelper;
 import com.adaptris.util.text.xml.XPath;
 
 public class PoiServiceTest extends ServiceCase {
   public static final String KEY_SAMPLE_INPUT = "poi.sample.input";
+  public static final String KEY_SAMPLE_INPUT_WITH_NULL = "poi.sample.input.null";
   private DefaultMessageFactory dMessageFactory = new DefaultMessageFactory();
-  
+
   public PoiServiceTest() {
     super();
   }
@@ -37,14 +44,14 @@ public class PoiServiceTest extends ServiceCase {
   protected Object retrieveObjectForCastorRoundTrip() {
     return new ExcelToXml();
   }
-  
+
   protected static byte[] readFile(String path) throws IOException {
     try (ByteArrayOutputStream out = new ByteArrayOutputStream(); InputStream in = new FileInputStream(new File(path))){
       IOUtils.copy(in, out);
-      return out.toByteArray();      
+      return out.toByteArray();
     }
   }
-  
+
   @Test
   public void testService() throws Exception {
     AdaptrisMessage msg = dMessageFactory.newMessage(readFile(PROPERTIES.getProperty(KEY_SAMPLE_INPUT)));
@@ -54,18 +61,53 @@ public class PoiServiceTest extends ServiceCase {
       start(service);
       service.doService(msg);
       Document d = XmlHelper.createDocument(msg, DocumentBuilderFactoryBuilder.newInstance());
-      XPath xp = new XPath();
-      assertNotNull(xp.selectSingleNode(d, "/spreadsheet/sheet[@name='Sheet1']"));
-      assertTrue(xp.selectNodeList(d, "/spreadsheet/sheet[@name='Sheet1']/row/cell").getLength() > 0);
-      assertNull(xp.selectSingleNode(d, "/spreadsheet/sheet[@name='Sheet1']/row/cell[@position='A1']"));
+      assertDocument(d);
     }
     finally {
       stop(service);
     }
   }
-  
+
+  @Test
+  public void testServiceIgnoreNullRowsFalse() throws Exception {
+    AdaptrisMessage msg = dMessageFactory.newMessage(readFile(PROPERTIES.getProperty(KEY_SAMPLE_INPUT_WITH_NULL)));
+    ExcelToXml service = new ExcelToXml();
+    service.getXmlStyle().setXmlEncoding("UTF-8");
+    try {
+      start(service);
+      ServiceException serviceException = assertThrows(ServiceException.class, () -> service.doService(msg));
+      assertEquals(Exception.class, serviceException.getCause().getClass());
+      assertEquals("Unable to process row 5; it's null", serviceException.getCause().getMessage());
+    } finally {
+      stop(service);
+    }
+  }
+
+  @Test
+  public void testServiceIgnoreNullRowsTrue() throws Exception {
+    AdaptrisMessage msg = dMessageFactory.newMessage(readFile(PROPERTIES.getProperty(KEY_SAMPLE_INPUT_WITH_NULL)));
+    ExcelToXml service = new ExcelToXml();
+    service.getXmlStyle().setXmlEncoding("UTF-8");
+    service.setIgnoreNullRows(true);
+    try {
+      start(service);
+      service.doService(msg);
+      Document d = XmlHelper.createDocument(msg, DocumentBuilderFactoryBuilder.newInstance());
+      assertDocument(d);
+    } finally {
+      stop(service);
+    }
+  }
+
+  private void assertDocument(Document d) throws XPathExpressionException {
+    XPath xp = new XPath();
+    assertNotNull(xp.selectSingleNode(d, "/spreadsheet/sheet[@name='Sheet1']"));
+    assertTrue(xp.selectNodeList(d, "/spreadsheet/sheet[@name='Sheet1']/row/cell").getLength() > 0);
+    assertNull(xp.selectSingleNode(d, "/spreadsheet/sheet[@name='Sheet1']/row/cell[@position='A1']"));
+  }
+
   @Override
   public boolean isAnnotatedForJunit4() {
     return true;
-  }   
+  }
 }
